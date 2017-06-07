bd2e19dddc10610969c80ccb01859c56e073fbc2
Emanuele DelBono <emanuele@codiceplastico.com>
2017-06-07 12:25:21 +0200

    refactoring ;

--- src/app/main/reducers/users.js
+++ src/app/main/reducers/users.js
@@ -2,13 +2,10 @@ import buildReducer from '../../../redux/buildReducer'
 import R from 'ramda'
 import * as Action from '../action'
 
-const userEq = R.curry((id, u) => u.user_id === id)
-const mapUser = R.map(u => ({user_id: u.user_id, username: u.username, timer_id: u.timer_id}))
-
 export default buildReducer({
-  [Action.UsersLoaded]: (state, action) => ({ 
-    users: R.compose(mapUser, R.reject(userEq(state.user_id)))(action.payload) 
-  }),
+  [Action.UsersLoaded]: (state, action) => { 
+    return {users:R.map(u => ({user_id: u.user_id, username: u.username, timer_id: u.timer_id}), action.payload)}
+  },
   [Action.SelectedUsersChanged]: (state, action) => {
     return {
       users: state.users.map(u => {
