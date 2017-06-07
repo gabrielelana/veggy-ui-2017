2dfcecb03ea5181309d1dd114a6d89cc4d279f6b
Emanuele DelBono <emanuele@codiceplastico.com>
2017-06-07 16:04:48 +0200

    added tags on the run

--- src/app/main/reducers/tags.js
+++ src/app/main/reducers/tags.js
@@ -5,5 +5,21 @@ import * as Action from '../action'
 export default buildReducer({
   [Action.TagsLoaded]: (state, action) => ({ 
     tags: action.payload.map(t => ({tag: t.tag, pomodori: t.pomodori, duration: moment.duration(t.duration).humanize()}))
-  })
+  }),
+  [Action.PomodoroStarted]: (state, action) => {  
+
+    action.payload.tags.forEach(at => {
+      const tag = state.tags.find(st => st.tag === at)
+      if (tag) {
+        tag.pomodori = tag.pomodori + 1
+      } else {
+        state.tags.push({tag: at, pomodori: 1})
+      }
+    })
+
+    return {
+      tags: state.tags
+    }
+  },
+  
 })
